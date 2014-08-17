1e4ff01a83add95d16bb281418ea583ab6f6ad5f
Juan Walker <jpwalker@gmail.com>
2014-08-16 21:32:55 -0400

    twilio endpoint

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -1,2 +1,16 @@
+require 'twilio-ruby'
+
+include Webhookable
+ 
+after_filter :set_header
+  
+skip_before_action :verify_authenticity_token
+ 
 class TwilioController < ApplicationController
+  def voice
+    response = Twilio::TwiML::Response.new do |r|
+      r.Say "Thanks for calling store name. Please leave a message."
+    end
+  render_twiml response
+  end
 end
